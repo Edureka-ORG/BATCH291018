f5d3467530d265ce6e9048ffe1c49f30db69e7d0
svaduka <sainagaraju.vaduka@gmail.com>
2018-11-08 09:06:41 +0530

    Sqoop Commands

--- SparkDemo-Batch291018/src/main/scala/com/edureka/spark/WordCountDriver.scala
+++ SparkDemo-Batch291018/src/main/scala/com/edureka/spark/WordCountDriver.scala
@@ -3,17 +3,24 @@ package com.edureka.spark
 import org.apache.spark.SparkConf
 import org.apache.spark.SparkContext
 
+// spark2-submit --verbose --master yarn --deploy-mode client --class com.edureka.spark.WordCountDriver sparkdemo-batch291018_2.11-1.0-SNAPSHOT.jar hdfs:/user/edureka_<YOUR-ID>/WordCount.txt
 object WordCountDriver {
   
   def main(args: Array[String]): Unit = {
+    
+    println("WordCount-Spark");
     
     val sparkConf = new SparkConf();
     sparkConf.setAppName(WordCountDriver.getClass.getName);
     
     val sc = new SparkContext(sparkConf);
     
-    val input = "hdfs:/user/edureka_144865/WordCount.txt";
+    val input = args(0);
+    println("Input is :"+input)
     val inputRDD = sc.textFile(input);
+    
+    inputRDD.collect().foreach(println);
+    
     
   }
   
